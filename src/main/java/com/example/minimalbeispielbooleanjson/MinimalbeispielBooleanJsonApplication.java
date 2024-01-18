fa5c878f8de9023a3b27767224af8bdcefb72b92
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2024-01-18 10:49:56 +0100

    Setup project (spring initializr web+lombok)

---
+++ src/main/java/com/example/minimalbeispielbooleanjson/MinimalbeispielBooleanJsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.minimalbeispielbooleanjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalbeispielBooleanJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinimalbeispielBooleanJsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minimalbeispielbooleanjson/MinimalbeispielBooleanJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minimalbeispielbooleanjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalbeispielBooleanJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
