176267f52fcdfe7c5e1e357b93e1d0f63ed8ae43
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2024-01-18 10:54:17 +0100

    Working POST (with record)

--- src/main/java/com/example/minimalbeispielbooleanjson/MinimalbeispielBooleanJsonApplication.java
+++ src/main/java/com/example/minimalbeispielbooleanjson/MinimalbeispielBooleanJsonApplication.java
@@ -2,6 +2,9 @@ package com.example.minimalbeispielbooleanjson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MinimalbeispielBooleanJsonApplication {
@@ -10,4 +13,17 @@ public class MinimalbeispielBooleanJsonApplication {
         SpringApplication.run(MinimalbeispielBooleanJsonApplication.class, args);
     }
 
+}
+
+record Cat(
+        boolean alive
+) {
+}
+
+@RestController
+class Controller {
+    @PostMapping
+    void post(@RequestBody Cat cat) {
+        System.out.println(cat.alive());
+    }
 }
